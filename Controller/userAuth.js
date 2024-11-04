879fd45a5a44608dbe0de750376a37bb8be492df
ayush gupta <115782080+aayu1720@users.noreply.github.com>
2024-11-04 08:39:17 +0530

    bcrypt change

--- Controller/userAuth.js
+++ Controller/userAuth.js
@@ -1,5 +1,5 @@
 const User = require("../Schema/user.schema");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 
 const registerUser  = async (req, res) => {
